c3688c5d7b67a22d4c035702c4cf1691e9b2b654
Haku <hakudevtw@gmail.com>
2025-02-18 09:14:48 +0900

    feat(design-system): integrate Toaster component into DesignSystemProvider

--- packages/design-system/index.tsx
+++ packages/design-system/index.tsx
@@ -1,9 +1,15 @@
 import type { ThemeProviderProps } from "next-themes";
 import { ThemeProvider } from "./providers/theme";
+import { Toaster } from "./components/ui/sonner";
 
 type DesignSystemProviderProperties = ThemeProviderProps;
 
 export const DesignSystemProvider = ({
   children,
   ...properties
-}: DesignSystemProviderProperties) => <ThemeProvider {...properties}>{children}</ThemeProvider>;
+}: DesignSystemProviderProperties) => (
+  <ThemeProvider {...properties}>
+    {children}
+    <Toaster />
+  </ThemeProvider>
+);
